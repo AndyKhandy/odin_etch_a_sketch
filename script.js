d190c7658628864f7ece52c691f58d037948b953
AndyKhandy <177374520+AndyKhandy@users.noreply.github.com>
2025-09-19 19:28:28 -0500

    Finish functionality of project

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const box = document.querySelector(".container");
-const sizeBtn = document.querySelector("#btn");
+
+const sizeBtn = document.querySelector("#btnChange");
+const clearBtn = document.querySelector("#btnClear");
 const sizeText = document.querySelector("#sizeText");
+const deactiveBtn = document.querySelector("#btnDeactivate")
+
 let boxSize = 16;
 let divs;
 
 let rainbow = false;
+let colorActive = true;
 
 displayBox();
 
 sizeBtn.addEventListener("click", () => {
-    clearBox();
+    deleteBox();
     boxSize = prompt("Enter how many squares you want per row! (max is 100)");
     displayBox();
 });
+
+clearBtn.addEventListener("click", () => {
+    deleteBox();
+    displayBox();
+});
+
+deactiveBtn.addEventListener("click", () => colorActive = !colorActive);
 
 function displayBox()
 {
@@ -20,7 +32,7 @@ function displayBox()
     for(let i = 1; i <= (boxSize*boxSize);i++)
     {
         const div = document.createElement("div");
-        div.style.width = `${480/boxSize}px`;
+        div.style.width = `${640/boxSize}px`;
         div.style.height = "auto";
         div.id = "box";
         div.addEventListener("mousemove", hoverBox);
@@ -32,19 +44,22 @@ function displayBox()
 
 function hoverBox(div)
 {
-    if(rainbow)
+    if(colorActive)
+    {
+        if(rainbow)
     {
         let randomRed = Math.floor(Math.random() * 255);
         let randomGreen = Math.floor(Math.random() * 255);
         let randomBlue = Math.floor(Math.random() * 255);
         this.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`
     }
-    else{
+        else{
          this.style.backgroundColor = "red";
+        }
     }
 }
 
-function clearBox()
+function deleteBox()
 {
     for(const div of divs)
     {
